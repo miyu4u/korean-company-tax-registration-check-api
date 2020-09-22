ce811571d0093e4334900fbd86f4b09b0b7fb38e
isaac <si.hyeon.chan@gmail.com>
2020-09-22 21:33:38 +0900

    최초 작성

---
+++ src/app.module.ts
@@ -0,0 +1,7 @@
+import { Module } from '@nestjs/common';
+import { CompanyNumberModule } from './company-number/company-number.module';
+
+@Module({
+  imports: [CompanyNumberModule],
+})
+export class AppModule { }
