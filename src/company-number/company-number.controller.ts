acb42231d273d5fb1a549f3a44b54775859f232b
isaac <si.hyeon.chan@gmail.com>
2020-09-22 23:24:23 +0900

    사업자 번호 체크 시 GET 파라미터로 처리하도록 변경

--- src/company-number/company-number.controller.ts
+++ src/company-number/company-number.controller.ts
@@ -1,15 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { CompanyNumberService } from './company-number.service';
 
 @Controller('company-number')
 export class CompanyNumberController {
 
     constructor(
-        protected service:CompanyNumberService
-    ){}
-    
-    @Get()
-    async test() {
-        return await this.service.check(1)
+        protected service: CompanyNumberService
+    ) { }
+
+    @Get(":num")
+    async test(@Param("num") num:number) {
+        return await this.service.check(num)
     }
 }
--- src/company-number/company-number.service.ts
+++ src/company-number/company-number.service.ts
@@ -1,7 +1,6 @@
 import { HttpService, Inject, Injectable } from '@nestjs/common';
 import { checker } from 'src/checker.interface';
 import Axios, { AxiosInstance } from 'axios'
-
 @Injectable()
 export class CompanyNumberService implements checker {
     constructor(
@@ -9,16 +8,25 @@ export class CompanyNumberService implements checker {
     ) { }
 
     async check(input: number): Promise<any> {
+        const payload = this.createPayload(input)
+        const response = await this.http.post("", payload)
+        const body = response.data
+        const status = (body.match(/<smpcBmanTrtCntn>(.*?)<\/smpcBmanTrtCntn>/)[1] as string).trimEnd()
+        const description = body.match(/<trtCntn>(.*?)<\/trtCntn>/)[1]
+        return { status, description }
+    }
+
+    protected createPayload(input: number): string {
         let payload = ``
         payload += `<map id="ATTABZAA001R08">`
         payload += `<pubcUserNo/>`
         payload += `<mobYn>N</mobYn>`
         payload += `<inqrTrgtClCd>1</inqrTrgtClCd>`
         payload += `<txprDscmNo>${input}</txprDscmNo>`
-        payload += `<dongCode>${input.toString().slice(3,4)}</dongCode>`
+        payload += `<dongCode>${input.toString().slice(3, 4)}</dongCode>`
         payload += `<psbSearch>Y</psbSearch>`
         payload += `<map id="userReqInfoVO"/></map>`
         payload += `<nts<nts>nts>59Tlkq16T5UT5vmp6rd1Fmp1qHPy3ObOjYy7GT5xMA48`
-        return (await this.http.post("", payload)).data
+        return payload
     }
 }
