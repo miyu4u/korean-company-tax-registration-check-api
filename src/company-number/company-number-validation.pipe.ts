42ac8c5ccdfbef1436399e42949a81b76c73dd6f
isaac <si.hyeon.chan@gmail.com>
2020-09-24 09:37:23 +0900

    validator 추가

---
+++ src/company-number/company-number-validation.pipe.ts
@@ -0,0 +1,12 @@
+import { BadRequestException, Injectable, PipeTransform } from "@nestjs/common";
+
+
+@Injectable()
+export class CompanyNumberValidationPipe implements PipeTransform<any, any> {
+    transform(value: any) {
+        if (!value) throw new BadRequestException("사업자 번호를 파리미터에 입력해주세요")
+        if (!String(value).match(/^\d{10}$/)) throw new BadRequestException("사업자 번호 양식이 올바르지 않습니다")
+        return Number(value)
+    }
+
+}
--- src/company-number/company-number.controller.ts
+++ src/company-number/company-number.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get, Param } from '@nestjs/common';
+import { CompanyNumberValidationPipe } from './company-number-validation.pipe';
 import { CompanyNumberService } from './company-number.service';
 
 @Controller('company-number')
@@ -9,7 +10,7 @@ export class CompanyNumberController {
     ) { }
 
     @Get(":num")
-    async test(@Param("num") num:number) {
+    async test(@Param("num",CompanyNumberValidationPipe) num:number) {
         return await this.service.check(num)
     }
 }
