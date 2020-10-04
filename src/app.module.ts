0d58a3f41e9567c92f3de467417a04a1d1d007dd
isaac <si.hyeon.chan@gmail.com>
2020-10-04 09:33:15 +0900

    formatting, 명령어 수정, 의존성 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { CompanyNumberModule } from './company-number/company-number.module';
-
 @Module({
-  imports: [CompanyNumberModule],
+  imports: [
+    CompanyNumberModule
+  ],
 })
 export class AppModule { }
