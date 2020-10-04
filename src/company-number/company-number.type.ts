b13459085babcf6ca47192c32d909afaf5d46bdc
isaac <si.hyeon.chan@gmail.com>
2020-10-04 09:32:40 +0900

    taxtype 수정

--- src/company-number/company-number.type.ts
+++ src/company-number/company-number.type.ts
@@ -2,4 +2,4 @@ export type inputPayload = { num: number, dongCode: number }
 
 export type companyRunInStatus = "사업" | "휴업" | "폐업" | ""
 
-export type companyTaxType = "일반" | "간이" | "면세"
+export type companyTaxType = "일반" | "간이" | "면세" | ""
